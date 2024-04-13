8f255b24c59f4d910f3623a28126491ae3568591
Petar Juric <petarjuricbepo@yahoo.com>
2024-04-13 12:29:54 +0000

    LCD display integration

--- main.cpp
+++ main.cpp
@@ -2,21 +2,22 @@
 #include "C12832.h"                     // LCD display library
 
 
-AnalogOut   aout(p18);                    // Application Board 3.5mm jack
-AnalogIn    pot1(p17);                     // Potentiometer connected to p17
-C12832      lcd(p5, p7, p6, p8, p11);        // LCD display
-BusIn       joy(p15, p12, p13, p16);
-DigitalIn   press(p14);
+AnalogOut   aout(p18);                  // Application Board 3.5mm jack
+AnalogIn    pot1(p17);                  // Potentiometer connected to p17
+C12832      lcd(p5, p7, p6, p8, p11);   // LCD display
+BusIn       up(p15);                    // Application Board Up
+BusIn       down(p12);                  // .. Down
+DigitalIn   press(p14);                 // Application Board joystick click
 
 class FnGenerator {
 public:
-    static const double PI;
+    static const float PI;
     static AnalogOut aout;
     static const int TSIZE = 4096;
     static float waveTable[TSIZE];
     static volatile int index;
-    static volatile double frequency;   // Add frequency control
-    static double phaseIncrement;       // To control the rate of index increment
+    static volatile float frequency;   
+    static float phaseIncrement;       // To control the rate of index increment
 
     static void initSineTable() {
         for (int i = 0; i < TSIZE; ++i) {
@@ -37,7 +38,7 @@ public:
         index = (index + int(phaseIncrement)) % TSIZE;
     }
 
-    static void updateFrequency(double newFrequency, float sampleRate) {
+    static void updateFrequency(float newFrequency, float sampleRate) {
         frequency = newFrequency;
         phaseIncrement = frequency / sampleRate * TSIZE;  
     }
@@ -45,15 +46,15 @@ public:
 
 // Initialize static members
 AnalogOut FnGenerator::aout(p18);
-static const int sampleRate = 44100;
-const double FnGenerator::PI = 3.14159265358979323846;
+static const int sampleRate = 20000;
+const float FnGenerator::PI = 3.14159265358979323846;
 float FnGenerator::waveTable[FnGenerator::TSIZE];
 volatile int FnGenerator::index = 0;
-volatile double FnGenerator::frequency = 28.0;
-double FnGenerator::phaseIncrement = FnGenerator::frequency / 44100 * FnGenerator::TSIZE;
+volatile float FnGenerator::frequency = 0;
+float FnGenerator::phaseIncrement = FnGenerator::frequency / 44100 * FnGenerator::TSIZE;
 
 int main() {
-    int fnLabelPos = 0;                     //0 sine, 1 square, 3 triangle, 4 noise
+    int fnLabelPos = 0;                 //0 sine, 1 square, 3 triangle, 4 noise
     FnGenerator::initSineTable();
     //FnGenerator::initSquareTable();
     Ticker ticker;
@@ -62,9 +63,26 @@ int main() {
 
 
     while (true) {
-
+        if(up && fnLabelPos != 0){
+            ticker.detach();
+            lcd.cls();
+            lcd.locate(0, 0);
+            lcd.printf("Sine");
+            FnGenerator::initSineTable();
+            ticker.attach(&FnGenerator::updateOutput, 1.0 / sampleRate);
+            fnLabelPos -= 1;
+        }
+        else if(down && fnLabelPos != 1){
+            ticker.detach();
+            lcd.cls();
+            lcd.locate(0, 0);
+            lcd.printf("Square");
+            FnGenerator::initSquareTable();
+            ticker.attach(&FnGenerator::updateOutput, 1.0 / sampleRate);
+            fnLabelPos += 1;
+        }
         float readValue = pot1.read();  
-        double newFrequency = 28.0 + double(readValue * 1000);  
+        float newFrequency = float(readValue * 999);  
         FnGenerator::updateFrequency(newFrequency, sampleRate);
         
         lcd.locate(0, 15);
